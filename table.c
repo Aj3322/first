ce4da4f8283f221b7fbe7301203cb493cf57f84c
AJAY KUMAR <114848454+Aj3322@users.noreply.github.com>
2022-10-02 11:54:54 +0530

    Add files via upload

---
+++ table.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+int main()
+{
+    int a;
+    int b=1,c=2,d=3,s=4,e=5,f=6,g=7,h=8,i=9,j=10;
+// code for table
+    printf("ENTER A NUMBER YOU WANT MULTIPLICATIN TABLE\n");
+    scanf("%d",&a);
+
+    printf("%d x 1 = %d\n",a,a*b);
+    printf("%d x 2 = %d\n",a,a*c);
+    printf("%d x 3 = %d\n",a,a*d);
+    printf("%d x 4 = %d\n",a,a*s);
+    printf("%d x 5 = %d\n",a,a*e);
+    printf("%d x 6 = %d\n",a,a*f);
+    printf("%d x 7 = %d\n",a,a*g);
+    printf("%d x 8 = %d\n",a,a*h);
+    printf("%d x 9 = %d\n",a,a*i);
+    printf("%d x 10= %d\n",a,a*j);
+    
+    return 0;
+}
